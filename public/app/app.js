bed2418138ff30bd05fb892731ca0421fed072c1
Luis Carlos Sanchez Gonzalez <bipter@gmail.com>
2015-05-05 11:33:53 -0500

    [REF] Adición formulario

--- public/app/app.js
+++ public/app/app.js
@@ -51,6 +51,45 @@
             alert(JSON.stringify(vm.model), null, 2);
         }
     });
+    app.controller('prototypeFormCtrl', function prototypeFormCtrl(formlyVersion) {
+        var vm = this;
+        vm.onSubmit = onSubmit;
+        vm.title = 'Agregar Indicadores';
+        vm.env = {
+            angularVersion: angular.version.full,
+            formlyVersion: formlyVersion
+        };
+        vm.model = {
+        };
+        vm.formFields = [
+            {
+                key: 'municipiosProto',
+                type: 'select',
+                wrapper: 'loading',
+                templateOptions: {
+                    label: 'Municipio',
+                    options: [],
+                    valueProp: '_id',
+                    labelProp: 'properties.HRname',
+                    required: true,
+                    placeholder: 'Seleccione un municipio de la lista'
+                },
+                controller: /* @ngInject */ function ($scope, jsonService) {
+                    $scope.to.loading = jsonService.getJSON().then(function (response) {
+                        $scope.to.options = response.data;
+                        return response;
+                    });
+                }
+            }
+        ];
+
+        vm.originalFields = angular.copy(vm.fields);
+
+        // function definition
+        function onSubmit() {
+            alert(JSON.stringify(vm.model), null, 2);
+        }
+    });
     app.factory('jsonService', function jsonService($http) {
         return {
             getJSON: getJSON
